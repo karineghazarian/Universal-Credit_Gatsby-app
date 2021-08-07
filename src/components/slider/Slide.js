10f05c507599727b97d677ccf6bd9b1c2f1c97b0
Karine Ghazaryan <karineghazaryan97@gmail.com>
2021-08-07 20:56:52 +0400

    slider ui

--- src/components/slider/Slide.js
+++ src/components/slider/Slide.js
@@ -1,12 +1,22 @@
 import React from "react"
 import PropTypes from "prop-types"
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
+import * as styles from "./slide.module.css"
 
-const Slide = React.memo(({ slide }) => (
-  <div>
-    <img src={slide.cover.publicURL} alt={slide.caption} />
-    <h4>{slide.caption}</h4>
-  </div>
-))
+
+const Slide = React.memo(({ slide }) =>
+{
+  console.log(slide, "slide!");
+  const image = getImage(slide.cover)
+
+  return (
+    <div>
+      <GatsbyImage image={image} alt={slide.caption} className="slider-image" />
+      <p>{slide.caption}</p>
+    </div>
+  )
+
+})
 
 Slide.propTypes = {
   slide: PropTypes.shape({
--- src/components/slider/Slider.js
+++ src/components/slider/Slider.js
@@ -1,17 +1,41 @@
 import React from "react"
 import PropTypes from "prop-types"
-
-import * as styles from "./slider.module.css"
+import Carousel from "react-animated-carousel";
+import { Link } from "gatsby"
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
+import "./slider.css"
 import Slide from "./Slide"
 
-const Slider = React.memo(({ slider, title }) => (
-  <div className={styles.slider}>
-    Slider {title}
-    {slider?.map(slide => (
-      <Slide key={slide.id} slide={slide} />
-    ))}
-  </div>
-))
+const Slider = React.memo(({ slider, title }) => 
+{
+  console.log("mtav slider", slider)
+  const slides = slider?.map(slide =>
+  {
+    const image = getImage(slide.cover)
+    return (
+      <div>
+        <GatsbyImage image={image} alt={slide.caption} className="slider-image" />
+        <Link to="/home">
+          <h1>{slide.caption}</h1>
+        </Link>
+      </div>
+    )
+  });
+  console.log(slides, "slides", slider, "slider")
+
+  return (
+    <div className="hero-container">
+      {
+        slider.length > 1 ?
+          <Carousel slides={slides}
+            animationType="SLIDE"
+            duration={5000}
+            withNavigation /> :
+          <Slide slide={slider[0]} />
+      }
+    </div>
+  )
+})
 
 Slider.defaultProps = {
   title: ""

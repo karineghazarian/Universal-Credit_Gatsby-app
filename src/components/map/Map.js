d42cd3c820d286fd85e48b10ef81e7a6b66e6040
Karine Ghazaryan <karineghazaryan97@gmail.com>
2021-08-09 12:40:10 +0400

    map implement

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -3,7 +3,15 @@ import PropTypes from "prop-types"
 
 const Map = React.memo(({ src, title }) => (
   // console.log("Map: ", src)
-  <div>{src}</div>
+  <iframe
+    src={src}
+    width="100%"
+    height="600"
+    frameBorder="0"
+    style={{ display: "block", border: 0, marginBottom: 0 }}
+    allowFullScreen=""
+    title="map-address"
+  />
 ))
 
 Map.defaultProps = {
